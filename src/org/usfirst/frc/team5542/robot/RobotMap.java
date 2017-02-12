8ea30c6b45619abdb5f9aafddd004c95ce459e04
KraftLegos <kgilso176@gmail.com>
2017-02-12 14:34:33 -0600

    Made the drivetrain

--- src/org/usfirst/frc/team5542/robot/RobotMap.java
+++ src/org/usfirst/frc/team5542/robot/RobotMap.java
@@ -7,6 +7,35 @@ package org.usfirst.frc.team5542.robot;
  * floating around.
  */
 public class RobotMap {
+	
+	public static int lfMotor = 11;
+	public static int rfMotor = 10;
+	public static int lbMotor = 9;
+	public static int rbMotor = 8;
+	
+	//gyro
+	public static int gyro = 1;
+	
+	//limit switch
+	public static int limit = 0;
+	
+	//encoder
+	public static int enc1 = 2;
+	public static int enc2 = 3;
+	
+	//solenoid (controlled over the PCM)
+	public static int solenoid = 0;
+	
+	// Throwing motors
+	public static int ltMotor = 13;
+	public static int rtMotor = 12;
+	
+	//camera 1 motors
+	public static int panServo2 = 3;
+	public static int tiltservo2 = 4;
+	
+	//Lift motors
+	public static int liftMotor = 14;
 	// For example to map the left and right motors, you could define the
 	// following variables to use with your drivetrain subsystem.
 	// public static int leftMotor = 1;
--- src/org/usfirst/frc/team5542/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team5542/robot/subsystems/DriveTrain.java
@@ -1,13 +1,22 @@
 package org.usfirst.frc.team5542.robot.subsystems;
 
+import org.usfirst.frc.team5542.robot.RobotMap;
+
+import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class DriveTrain extends Subsystem {
-	// Put methods for controlling this subsystem
-	// here. Call these from Commands.
+	private RobotDrive myDrive;
+	private CANTalon lfMotor, rfMotor, lbMotor, rbMotor;
+	
+	private DriveTrain() {
+		lfMotor = new CANTalon(RobotMap.lfMotor);
+	}
 
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
